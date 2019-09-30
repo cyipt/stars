42015c378b7777fec3cb238576a43d80226f8c7f
joeytalbot <joeytalbot85@gmail.com>
2019-09-30 22:03:54 +0100

    Survey data input from excel

--- scripts/read.R
+++ scripts/read.R
@@ -1,37 +1,99 @@
 library(readxl)
-library(tidyr)
+# library(tidyr)
 library(dplyr)
+# library(stringr)
+library(lubridate)
 
 # bedford_entry = read.csv("../stars/luton-survey/bedford-entry.csv")
 # bedford_exit = read.csv("../stars/luton-survey/bedford-exit.csv")
 
-bedford_entry = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Bedford!A9:I37")
+bedford_entry = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Bedford!A9:I35",col_types = c("date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 dim(bedford_entry)
 bedford_entry = bedford_entry %>% rename(Time = ...1,OnFoot = "On Foot",PrivateCarDropOff = "Private Car Drop off")
-bedford_entry = drop_na(bedford_entry)
+bedford_entry$Time = hms::as_hms(bedford_entry$Time)
+# bedford_entry = drop_na(bedford_entry)
+# bedford_entry_hourly = bedford_entry[str_which(bedford_entry$Time, "Hr"),]
+# bedford_entry_15min = bedford_entry[str_which(bedford_entry$Time, "0"),]
+bedford_entry_hourly = bedford_entry %>% 
+  filter(is.na(Time))
+bedford_entry_15min = bedford_entry %>% 
+  filter(!is.na(Time))
+bedford_entry_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
 
-bedford_exit = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Bedford!K9:R37") 
-bedford_exit = bedford_exit %>% rename(Time = ...1,OnFoot = "On Foot",PrivateCarDropOff = "Private Car Drop off")
-bedford_exit = cbind(bedford_entry$Time,bedford_exit)
-bedford_exit = drop_na(bedford_exit)
 
-bedford_sj_entry = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Bedford St John!A9:H37")
-bedford_sj_entry = drop_na(bedford_sj_entry)
 
-bedford_sj_exit = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Bedford St John!J9:P37") 
-bedford_sj_exit = drop_na(bedford_sj_exit)
+bedford_exit = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Bedford!K9:R35") 
+bedford_exit = bedford_exit %>% rename(OnFoot = "On Foot",PrivateCarPickUp = "Private Car Pick Up")
+# bedford_exit = drop_na(bedford_exit)
+bedford_exit = bind_cols(Time = bedford_entry$Time,bedford_exit)
+bedford_exit_hourly = bedford_exit %>% 
+  filter(is.na(Time))
+bedford_exit_15min = bedford_exit %>% 
+  filter(!is.na(Time))
+bedford_exit_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
 
-flitwick_entry = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Flitwick!A9:I37")
-flitwick_entry = drop_na(flitwick_entry)
 
-flitwick_exit = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Flitwick!K9:R37") 
-flitwick_exit = drop_na(flitwick_exit)
+bedford_sj_entry = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Bedford St John!A9:H35",col_types = c("date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+dim(bedford_sj_entry)
+bedford_sj_entry = bedford_sj_entry %>% rename(Time = ...1,OnFoot = "On Foot",PrivateCarDropOff = "Private Car Drop off")
+bedford_sj_entry$Time = hms::as_hms(bedford_sj_entry$Time)
+bedford_sj_entry_hourly = bedford_sj_entry %>% 
+  filter(is.na(Time))
+bedford_sj_entry_15min = bedford_sj_entry %>% 
+  filter(!is.na(Time))
+bedford_sj_entry_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
 
-harlington_entry = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Harlington!A9:I37")
-harlington_entry = drop_na(harlington_entry)
 
-harlington_exit = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Harlington!K9:R37") 
-harlington_exit = drop_na(harlington_exit)
+
+
+bedford_sj_exit = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Bedford St John!J9:P35") 
+bedford_sj_exit = bedford_sj_exit %>% rename(OnFoot = "On Foot",PrivateCarPickUp = "Private Car Pick Up")
+bedford_sj_exit = bind_cols(Time = bedford_sj_entry$Time,bedford_sj_exit)
+bedford_sj_exit_hourly = bedford_sj_exit %>% 
+  filter(is.na(Time))
+bedford_sj_exit_15min = bedford_sj_exit %>% 
+  filter(!is.na(Time))
+bedford_sj_exit_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
+
+
+
+flitwick_entry = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Flitwick!A9:I35",col_types = c("date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+dim(flitwick_entry)
+flitwick_entry = flitwick_entry %>% rename(Time = ...1,OnFoot = "On Foot",PrivateCarDropOff = "Private Car Drop off")
+flitwick_entry$Time = hms::as_hms(flitwick_entry$Time)
+flitwick_entry_hourly = flitwick_entry %>% 
+  filter(is.na(Time))
+flitwick_entry_15min = flitwick_entry %>% 
+  filter(!is.na(Time))
+flitwick_entry_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
+
+flitwick_exit = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Flitwick!K9:R35") 
+flitwick_exit = flitwick_exit %>% rename(OnFoot = "On Foot",PrivateCarPickUp = "Private Car Pick Up")
+flitwick_exit = bind_cols(Time = flitwick_entry$Time,flitwick_exit)
+flitwick_exit_hourly = flitwick_exit %>% 
+  filter(is.na(Time))
+flitwick_exit_15min = flitwick_exit %>% 
+  filter(!is.na(Time))
+flitwick_exit_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
+
+harlington_entry = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Harlington!A9:I35",col_types = c("date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+dim(harlington_entry)
+harlington_entry = harlington_entry %>% rename(Time = ...1,OnFoot = "On Foot",PrivateCarDropOff = "Private Car Drop off")
+harlington_entry$Time = hms::as_hms(harlington_entry$Time)
+harlington_entry_hourly = harlington_entry %>% 
+  filter(is.na(Time))
+harlington_entry_15min = harlington_entry %>% 
+  filter(!is.na(Time))
+harlington_entry_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
+
+harlington_exit = read_excel("../stars/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Harlington!K9:R35") 
+harlington_exit = harlington_exit %>% rename(OnFoot = "On Foot",PrivateCarPickUp = "Private Car Pick Up")
+harlington_exit = bind_cols(Time = harlington_entry$Time,harlington_exit)
+harlington_exit_hourly = harlington_exit %>% 
+  filter(is.na(Time))
+harlington_exit_15min = harlington_exit %>% 
+  filter(!is.na(Time))
+harlington_exit_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
 
 ################
 
