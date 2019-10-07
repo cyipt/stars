f4c4ab572d0b8787bcde4023fb428730acfbc54e
joeytalbot <joeytalbot85@gmail.com>
2019-10-07 11:31:36 +0100

    Scatterplot
    Added scatterplot of ONS v survey data for station usage

--- scripts/read.R
+++ scripts/read.R
@@ -1,8 +1,5 @@
-# library(readxl)
-library(tidyr)
-# library(dplyr)
-# # library(stringr)
-# library(lubridate)
+library(readxl)
+library(lubridate)
 library(tidyverse)
 
 # bedford_entry = read.csv("../stars-data/luton-survey/bedford-entry.csv")
@@ -96,6 +93,72 @@ harlington_exit_15min = harlington_exit %>%
   filter(!is.na(Time))
 harlington_exit_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
 
+
+leagrave_entry = read_excel("../stars-data/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Leagreave!A9:H35",col_types = c("date","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+dim(leagrave_entry)
+leagrave_entry = leagrave_entry %>% rename(Time = ...1,OnFoot = "On Foot",PrivateCarDropOff = "Private Car Drop off")
+leagrave_entry$Time = hms::as_hms(leagrave_entry$Time)
+leagrave_entry_hourly = leagrave_entry %>% 
+  filter(is.na(Time))
+leagrave_entry_15min = leagrave_entry %>% 
+  filter(!is.na(Time))
+leagrave_entry_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
+
+
+leagrave_exit = read_excel("../stars-data/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Leagreave!J9:P35") 
+leagrave_exit = leagrave_exit %>% rename(OnFoot = "On Foot",PrivateCarPickUp = "Private Car Pick Up")
+leagrave_exit = bind_cols(Time = leagrave_entry$Time,leagrave_exit)
+leagrave_exit_hourly = leagrave_exit %>% 
+  filter(is.na(Time))
+leagrave_exit_15min = leagrave_exit %>% 
+  filter(!is.na(Time))
+leagrave_exit_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
+
+
+
+luton_airpt_pkwy_entry = read_excel("../stars-data/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Luton Airport Parkway!A9:I35",col_types = c("date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+dim(luton_airpt_pkwy_entry)
+luton_airpt_pkwy_entry = luton_airpt_pkwy_entry %>% rename(Time = ...1,OnFoot = "On Foot",PrivateCarDropOff = "Private Car Drop off")
+luton_airpt_pkwy_entry$Time = hms::as_hms(luton_airpt_pkwy_entry$Time)
+luton_airpt_pkwy_entry_hourly = luton_airpt_pkwy_entry %>% 
+  filter(is.na(Time))
+luton_airpt_pkwy_entry_15min = luton_airpt_pkwy_entry %>% 
+  filter(!is.na(Time))
+luton_airpt_pkwy_entry_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
+
+luton_airpt_pkwy_exit = read_excel("../stars-data/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Luton Airport Parkway!K9:R35") 
+luton_airpt_pkwy_exit = luton_airpt_pkwy_exit %>% rename(OnFoot = "On Foot",PrivateCarPickUp = "Private Car Pick Up")
+luton_airpt_pkwy_exit = bind_cols(Time = luton_airpt_pkwy_entry$Time,luton_airpt_pkwy_exit)
+luton_airpt_pkwy_exit_hourly = luton_airpt_pkwy_exit %>% 
+  filter(is.na(Time))
+luton_airpt_pkwy_exit_15min = luton_airpt_pkwy_exit %>% 
+  filter(!is.na(Time))
+luton_airpt_pkwy_exit_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
+
+
+
+
+luton_entry = read_excel("../stars-data/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Luton!A9:I35",col_types = c("date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+dim(luton_entry)
+luton_entry = luton_entry %>% rename(Time = ...1,OnFoot = "On Foot",PrivateCarDropOff = "Private Car Drop off")
+luton_entry$Time = hms::as_hms(luton_entry$Time)
+luton_entry_hourly = luton_entry %>% 
+  filter(is.na(Time))
+luton_entry_15min = luton_entry %>% 
+  filter(!is.na(Time))
+luton_entry_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
+
+luton_exit = read_excel("../stars-data/luton-survey/Luton-Rail-Stations-Reports/1335-WTR_EntryExit_1-7_27th-29thNov.xlsx", range = "Luton!K9:R35") 
+luton_exit = luton_exit %>% rename(OnFoot = "On Foot",PrivateCarPickUp = "Private Car Pick Up")
+luton_exit = bind_cols(Time = luton_entry$Time,luton_exit)
+luton_exit_hourly = luton_exit %>% 
+  filter(is.na(Time))
+luton_exit_15min = luton_exit %>% 
+  filter(!is.na(Time))
+luton_exit_hourly$Time = hm(c("05:00","06:00","07:00","08:00","09:00","10:00"))
+
+
+
 ##########Histogram of mode and time######
 
 # bedford_all_15min = inner_join(bedford_entry_15min,bedford_exit_15min,by = bedford_entry_15min$Time)
@@ -116,13 +179,78 @@ ggplot(bedford_exit_15min,aes(col=Mode,Time,Exits)) + geom_freqpoly(stat = "iden
 
 #######Entries and exits together
 
-bedford_all_hourly = bind_cols(bedford_entry_hourly,bedford_exit_hourly[2:9])
+bedford_all_hourly = bind_cols(bedford_entry_hourly,bedford_exit_hourly[2:ncol(bedford_exit_hourly)])
 
 bedford_all_hourly = bedford_all_hourly %>%
   mutate(Car = (Car+Car1),Cycle=(Cycle+Cycle1),PrivateCarDropOff=(PrivateCarDropOff+PrivateCarPickUp),Bus=(Bus+Bus1),Taxi=(Taxi+Taxi1),Motorcycle=(Motorcycle+Motorcycle1),OnFoot=(OnFoot+OnFoot1),Total=(Total+Total1)) %>%
   select(-ends_with("1"),-PrivateCarPickUp)
 
-barplot(bedford_all_hourly$Total,xlab = "Time")
+sum(bedford_all_hourly$Total)
+
+bedford_sj_all_hourly = bind_cols(bedford_sj_entry_hourly,bedford_sj_exit_hourly[2:ncol(bedford_sj_exit_hourly)])
+
+bedford_sj_all_hourly = bedford_sj_all_hourly %>%
+  mutate(Car = (Car+Car1),Cycle=(Cycle+Cycle1),PrivateCarDropOff=(PrivateCarDropOff+PrivateCarPickUp),Taxi=(Taxi+Taxi1),Motorcycle=(Motorcycle+Motorcycle1),OnFoot=(OnFoot+OnFoot1),Total=(Total+Total1)) %>%
+  select(-ends_with("1"),-PrivateCarPickUp)
+
+sum(bedford_sj_all_hourly$Total)
+
+flitwick_all_hourly = bind_cols(flitwick_entry_hourly,flitwick_exit_hourly[2:ncol(flitwick_exit_hourly)])
+
+flitwick_all_hourly = flitwick_all_hourly %>%
+  mutate(Car = (Car+Car1),Cycle=(Cycle+Cycle1),PrivateCarDropOff=(PrivateCarDropOff+PrivateCarPickUp),Taxi=(Taxi+Taxi1),Motorcycle=(Motorcycle+Motorcycle1),OnFoot=(OnFoot+OnFoot1),Total=(Total+Total1)) %>%
+  select(-ends_with("1"),-PrivateCarPickUp)
+
+sum(flitwick_all_hourly$Total)
+
+harlington_all_hourly = bind_cols(harlington_entry_hourly,harlington_exit_hourly[2:ncol(harlington_exit_hourly)])
+
+harlington_all_hourly = harlington_all_hourly %>%
+  mutate(Car = (Car+Car1),Cycle=(Cycle+Cycle1),PrivateCarDropOff=(PrivateCarDropOff+PrivateCarPickUp),Taxi=(Taxi+Taxi1),Motorcycle=(Motorcycle+Motorcycle1),OnFoot=(OnFoot+OnFoot1),Total=(Total+Total1)) %>%
+  select(-ends_with("1"),-PrivateCarPickUp)
+
+sum(harlington_all_hourly$Total)
+
+leagrave_all_hourly = bind_cols(leagrave_entry_hourly,leagrave_exit_hourly[2:ncol(leagrave_exit_hourly)])
+
+leagrave_all_hourly = leagrave_all_hourly %>%
+  mutate(Car = (Car+Car1),Cycle=(Cycle+Cycle1),PrivateCarDropOff=(PrivateCarDropOff+PrivateCarPickUp),Taxi=(Taxi+Taxi1),Motorcycle=(Motorcycle+Motorcycle1),OnFoot=(OnFoot+OnFoot1),Total=(Total+Total1)) %>%
+  select(-ends_with("1"),-PrivateCarPickUp)
+
+sum(leagrave_all_hourly$Total)
+
+luton_airpt_pkwy_all_hourly = bind_cols(luton_airpt_pkwy_entry_hourly,luton_airpt_pkwy_exit_hourly[2:ncol(luton_airpt_pkwy_exit_hourly)])
+
+luton_airpt_pkwy_all_hourly = luton_airpt_pkwy_all_hourly %>%
+  mutate(Car = (Car+Car1),Cycle=(Cycle+Cycle1),PrivateCarDropOff=(PrivateCarDropOff+PrivateCarPickUp),Taxi=(Taxi+Taxi1),Motorcycle=(Motorcycle+Motorcycle1),OnFoot=(OnFoot+OnFoot1),Total=(Total+Total1)) %>%
+  select(-ends_with("1"),-PrivateCarPickUp)
+
+sum(luton_airpt_pkwy_all_hourly$Total)
+
+luton_all_hourly = bind_cols(luton_entry_hourly,luton_exit_hourly[2:ncol(luton_exit_hourly)])
+
+luton_all_hourly = luton_all_hourly %>%
+  mutate(Car = (Car+Car1),Cycle=(Cycle+Cycle1),PrivateCarDropOff=(PrivateCarDropOff+PrivateCarPickUp),Taxi=(Taxi+Taxi1),Motorcycle=(Motorcycle+Motorcycle1),OnFoot=(OnFoot+OnFoot1),Total=(Total+Total1)) %>%
+  select(-ends_with("1"),-PrivateCarPickUp)
+
+sum(luton_all_hourly$Total)
+
+##############Compare survey results with ORR 2017-18 data########
+
+orr_data = read.csv("../stars-data/data/orr/beds_stations.csv")
+orr_data$EntriesAndExits_1718 = orr_data$EntriesAndExits_1718/1000000
+
+compare = tibble(Station_name = orr_data$Station.Name, EntriesAndExits_1718 = orr_data$EntriesAndExits_1718,survey = c(sum(bedford_all_hourly$Total),sum(bedford_sj_all_hourly$Total),sum(flitwick_all_hourly$Total),sum(harlington_all_hourly$Total),sum(leagrave_all_hourly$Total),sum(luton_all_hourly$Total),sum(luton_airpt_pkwy_all_hourly$Total)))
+
+
+ggplot(compare,aes(EntriesAndExits_1718,survey)) +
+  geom_point(col = "red") +
+  geom_text(aes(label = Station_name,hjust=-0.1)) +
+  xlab("ORR data 17_18 (million entries and exits)") + ylab("Bedfordshire survey data") +
+  coord_cartesian(xlim = c(0,6)) +
+  theme_grey()
+
+
 
 # bedford_all_hourly$Total = NULL
 # bedford_all_hourly = bedford_all_hourly %>% 
